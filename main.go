d9a1f70f2f8076b7c3bef31ba16c2b57177281d3
Spencer Luther <35349326+stl4018@users.noreply.github.com>
2020-11-11 15:40:32 -1000

    Upload files from local rep

---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// String constants for commands
+const (
+	BuildCommandString  = "build"
+	LaunchCommandString = "launch"
+	PullCommandString   = "pull="
+)
+
+func main() {
+
+	// Get commands and error from parser
+	commands, err := RaiseParser()
+	// Handle err
+	if err != nil {
+		fmt.Println("Error parsing script: " + err.Error())
+		return
+	}
+
+	// Send commands to router
+	err = RaiseRouter(commands)
+	// Handle err
+	if err != nil {
+		fmt.Println("Error executing commands: " + err.Error())
+		return
+	}
+
+	fmt.Println("Finished successfully")
+}
+
+/**********************************************
+// RaiseParser: Takes command line arguments and parses the pipeline commands
+// Input: None. (Uses os.Args)
+// Output: String array of commands, errors
+***********************************************/
+func RaiseParser() (commands []string, err error) {
+	isNonAlt := false
+	isAlt := false
+
+	//Iterate through os.Args
+	for argIndex, arg := range os.Args {
+		// Skip the command for running the program, "./raise"
+		if argIndex == 0 {
+			continue
+		}
+
+		// Parse pull commands
+		if len(arg) >= 7 && arg[0:6] == "-pull=" {
+			commands = append(commands, PullCommandString+arg[6:len(arg)])
+			isNonAlt = true
+			continue
+		} else if len(arg) >= 10 && arg[0:9] == "-AltPull=" {
+			commands = append(commands, PullCommandString+arg[9:len(arg)])
+			isAlt = true
+			continue
+		}
+
+		//Parse remaining commands
+		switch arg {
+		case "-build":
+			commands = append(commands, BuildCommandString)
+			isNonAlt = true
+		case "-launch":
+			commands = append(commands, LaunchCommandString)
+			isNonAlt = true
+		case "-AltBuildAndLaunch":
+			commands = append(commands, BuildCommandString)
+			commands = append(commands, LaunchCommandString)
+			isAlt = true
+		default:
+			err = errors.New("Error: Invalid Command")
+			return
+		}
+
+		// Check for script uniformity
+		if isNonAlt && isAlt {
+			err = errors.New("Error: multiple languages detected")
+		}
+	}
+
+	return
+}
+
+/**************************************************
+// Raise Router: Executes pipeline functionss for a given set of commands
+// Input: String array of commands
+// Output: Error thrown
+**************************************************/
+func RaiseRouter(commands []string) (err error) {
+	didBuild := false
+	didLaunch := false
+	improperOrderErr := errors.New("Error: Commands in an improper order")
+
+	if len(commands) <= 0 {
+		err = errors.New("Invalid number of commands parsed from script")
+		return
+	}
+
+	for _, command := range commands {
+
+		// Check pull commands
+		if strings.Contains(command, PullCommandString) {
+			if didBuild || didLaunch {
+				err = improperOrderErr
+				return
+			}
+			pullLocation := command[len(PullCommandString):len(command)]
+			executePull(pullLocation)
+			continue
+		}
+
+		// Check for remaining commands
+		switch command {
+		case BuildCommandString:
+			if didLaunch {
+				err = improperOrderErr
+				return
+			}
+			executeBuild()
+			didBuild = true
+		case LaunchCommandString:
+			executeLaunch()
+			didLaunch = true
+		default:
+			err = errors.New("Invalid command found")
+			return
+		}
+	}
+
+	return
+}
+
+/********************************************
+// executePull: Simulate a pull
+// Input: String of the website location to pull from
+// Output: None
+********************************************/
+func executePull(location string) {
+	fmt.Println("Pulling from " + location + " ...")
+}
+
+/********************************************
+// executeBuild: Simulate a build
+// Input: None
+// Output: None
+********************************************/
+func executeBuild() {
+	fmt.Println("Building ...")
+}
+
+/********************************************
+// executeLaunch: Simulate a build
+// Input: None
+// Output: None
+********************************************/
+func executeLaunch() {
+	fmt.Println("Launching ...")
+}
---
+++ main_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParser(t *testing.T) {
+
+	// Test no commands
+	os.Args = []string{"./raise"}
+	parseNoCommands, err := RaiseParser()
+	if err != nil {
+		t.Errorf("Error was not nil: %v", err)
+	}
+	if parseNoCommands.len() != 0 {
+		t.Errorf("Expected no commands, got %v instead", parseNoCommands)
+	}
+
+	// Test no-alt commands
+	os.Args = []string{"./raise_project", "-pull=somewhere.com", "-build", "-launch"}
+	parseNoAltCommands, err := RaiseParser()
+	if err != nil {
+		t.Errorf("Error was not nil: %v", err)
+	}
+	if parseNoAltCommands.len() != 3 || parseNoAltCommands[0] != "pull=somewhere.com"
+			|| parseNoAltCommands[1] != "build" || parseNoAltCommands[2] != "launch"  {
+		t.Errorf("Expected ordered no-alt commands, got %v instead", parseNoAltCommands)
+	}
+
+	// Test out of order no-alt commands
+	os.Args = []string{"./raise", "-pull=somewhere.com", "-launch", "-build"}
+	parseNoAltCommands, err = RaiseParser()
+	if err != nil {
+		t.Errorf("Error was not nil: %v", err)
+	}
+	if parseNoAltCommands.len() != 3 || parseNoAltCommands[0] != "pull=somewhere.com"
+			|| parseNoAltCommands[1] != "launch" || parseNoAltCommands[2] != "build"  {
+		t.Errorf("Expected un-ordered no-alt commands, got %v instead", parseNoAltCommands)
+	}
+
+	// Test alt commands
+	os.Args = []string{"./raise", "-AltPull=somewhere.com", "-AltBuildAndLaunch"}
+	parseAltCommands, err = RaiseParser()
+	if err != nil {
+		t.Errorf("Error was not nil: %v", err)
+	}
+	if parseAltCommands.len() != 3 || parseAltCommands[0] != "pull=somewhere.com"
+			|| parseAltCommands[1] != "launch" || parseAltCommands[2] != "build"  {
+		t.Errorf("Expected ordered alt commands, got %v instead", parseAltCommands)
+	}
+
+	// Test multi-language commands
+	os.Args = []string{"./raise", "-AltPull=somewhere.com", "-build"}
+	parseAltCommands, err = RaiseParser()
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	// Test illegible commands
+	os.Args = []string{"./raise", "-AltPull=somewhere.com", "-oogabooga"}
+	parseAltCommands, err = RaiseParser()
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+}
+
+func TestRouter(t *testing.T) {
+	// Test no commands
+	commands := []string{}
+	err := RaiseRouter(commands)
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	// Test ordered commands
+	commands = []string{"-build", "-launch"}
+	err = RaiseRouter(commands)
+	if err != nil {
+		t.Errorf("Expected no error but got %v", err)
+	}
+
+	// Test unordered commands
+	commands = []string{"-launch", "-build"}
+	err = RaiseRouter(commands)
+	if err != nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+}
